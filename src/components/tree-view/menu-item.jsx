06bfa15428e9e46a62b952cadc512fc539538e64
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-05 18:30:22 +0530

    Updating menu-item.jsx
    Updating menu-item.jsx

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
+import menus from "./data";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
+export default function MenuItem({ item }) {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
 
+  console.log(displayCurrentChildren);
 
-export default function MenuItem({item}){
+  return (
+    <li>
+      <div className="menu-item">
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? (
+              <FaMinus color="black" size={25} />
+            ) : (
+              <FaPlus color="#fff" size={25} />
+            )}
+          </span>
+        ) : null}
+      </div>
+      {/* <p>{item.label}</p> */}
 
-  return <div>Menu Item</div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
+        <MenuList list={item.children} />
+      ) : null}
+    </li>
+  );
 }
